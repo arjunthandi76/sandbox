5ff3a04b650f5470385a461357c78babc0c26a67
Arjun Thandi <arjunthandi@gmail.com>
2014-01-23 12:41:54 +0000

    just another test

--- sites/default/behat/features/bootstrap/FeatureContext.php
+++ sites/default/behat/features/bootstrap/FeatureContext.php
@@ -14,7 +14,7 @@ use Drupal\DrupalExtension\Context\DrupalContext;
 //
 //   require_once 'PHPUnit/Autoload.php';
 //   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+// 2nd time changing for git test
 
 /**
  * Features context.
